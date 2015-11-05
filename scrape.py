a6657772afb5a7835fc1a86fb8cb626ee1664a60
James T. Allen <j.allen@physics.usyd.edu.au>
2015-11-05 21:16:36 +1100

    Store regionSTCS information.

--- scrape.py
+++ scrape.py
@@ -8,6 +8,12 @@ def extract_image_metadata(header, title=None, image_url=None):
     wcs = WCS(header)
     center = 0.5*(header['NAXIS1'] - 1), 0.5*(header['NAXIS2'] - 1)
     center_wcs = wcs.wcs_pix2world([center[0]], [center[1]], 0)
+    corners_x = (0, 0, header['NAXIS1']-1, header['NAXIS1']-1)
+    corners_y = (0, header['NAXIS2']-1, header['NAXIS2']-1, 0)
+    corners_wcs = wcs.wcs_pix2world(corners_x, corners_y, 0)
+    regionSTCS = 'Polygon ICRS'
+    for wcs_x, wcs_y in zip(*corners_wcs):
+        regionSTCS = regionSTCS + ' {} {}'.format(wcs_x, wcs_y)
     metadata = ImageMetadata(
         title=title,
         ra_center=center_wcs[0][0],
@@ -23,6 +29,7 @@ def extract_image_metadata(header, title=None, image_url=None):
             wcs.wcs.cd[0, 0], wcs.wcs.cd[0, 1],
             wcs.wcs.cd[1, 0], wcs.wcs.cd[1, 1]),
         reference_frame=wcs.wcs.radesys,
+        regionSTCS=regionSTCS,
         )
     # metadata['inst'] = None # recommended
     # metadata['mjd_obs'] = None # recommended
