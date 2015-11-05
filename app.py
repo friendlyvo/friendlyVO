9a5026d29f5d47f30b9a36149f5a144a65dcf39e
AstroCate <catherine.braiding@gmail.com>
2015-11-05 16:04:39 +1100

    Mucking about with templates to make our html w3c compliant.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request
+from flask import render_template
 
 from query import query
 from ingest import ingest
@@ -9,6 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World! I can deploy automatically :)'
+
+@app.route('/test')
+def test():
+    page_title = 'the Friendly VO'
+    page_content = 'Yo, I got the stuff working.'
+    return render_template('index.html', 
+			   title = 'test page',
+			   page_title = page_title,
+ 			   page_content = page_content)
 
 app.register_blueprint(query, url_prefix='/query')
 
