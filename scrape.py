60a9f8e1ffb0439e742beaeeec9adad296b1b376
James T. Allen <j.allen@physics.usyd.edu.au>
2015-11-05 22:02:19 +1100

    Don't put brackets in database.

--- scrape.py
+++ scrape.py
@@ -19,13 +19,13 @@ def extract_image_metadata(header, title=None, image_url=None):
         ra_center=center_wcs[0][0],
         dec_center=center_wcs[1][0],
         naxes=wcs.naxis,
-        naxis='[{} {}]'.format(header['NAXIS1'], header['NAXIS2']),
-        scale='[{} {}]'.format(*proj_plane_pixel_scales(wcs)),
+        naxis='{} {}'.format(header['NAXIS1'], header['NAXIS2']),
+        scale='{} {}'.format(*proj_plane_pixel_scales(wcs)),
         format='image/fits',
         image_url=image_url,
-        crpix='[{} {}]'.format(*wcs.wcs.crpix),
-        crval='[{} {}]'.format(*wcs.wcs.crval),
-        cd_matrix='[{} {} {} {}]'.format(
+        crpix='{} {}'.format(*wcs.wcs.crpix),
+        crval='{} {}'.format(*wcs.wcs.crval),
+        cd_matrix='{} {} {} {}'.format(
             wcs.wcs.cd[0, 0], wcs.wcs.cd[0, 1],
             wcs.wcs.cd[1, 0], wcs.wcs.cd[1, 1]),
         reference_frame=wcs.wcs.radesys,
