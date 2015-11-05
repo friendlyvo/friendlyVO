9c58c6d7b28fd21d3521958cb2419ed9abeeb459
James T. Allen <j.allen@physics.usyd.edu.au>
2015-11-05 12:37:59 +1100

    Parrots back coordinates.

--- hello.py
+++ hello.py
@@ -1,8 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World! I can deploy automatically :)'
+
+@app.route('/query/sia')
+def sia():
+    ra = request.args.get('RA')
+    dec = request.args.get('Dec')
+    size = request.args.get('Size')
+    return 'You asked for RA = {}, Dec = {}, Size = {}'.format(ra, dec, size)
