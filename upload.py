f6744df613523ba83179dde98018f3a15b93b97e
James T. Allen <j.allen@physics.usyd.edu.au>
2015-11-05 18:54:17 +1100

    Add function for getting the header from a remote FITS file.

--- upload.py
+++ upload.py
@@ -1,11 +1,11 @@
-# from StringIO import StringIO
+import contextlib
 
 from flask import Blueprint, render_template, flash
 from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
-# import requests
+import requests
 from astropy.io import fits
 
 from scrape import extract_image_metadata
@@ -24,12 +24,11 @@ def new():
     form = NewForm()
     if form.validate_on_submit():
         url_list = form.url_list.data.split('\n')
+        # Strip \r and the last empty item. Terrible coding here.
+        url_list = [url[:-1] for url in url_list[:-1]]
         result_list = []
         for url in url_list:
-            # response = requests.get(url)
-            # if response.status_code != 200:
-            #     continue
-            header = fits.getheader(url)
+            header = get_remote_header(url)
             metadata = extract_image_metadata(header, image_url=url)
             add_image(metadata)
             result_list.append(metadata)
@@ -39,3 +38,17 @@ def new():
              for meta in result_list])
         # return redirect('/index')
     return render_template('new.html', title='Add new data', form=form)
+
+def get_remote_header(url):
+    with contextlib.closing(requests.get(url, stream=True)) as response:
+        if response.status_code != 200:
+            return None
+        header_str = ''
+        for idx, line in enumerate(response.iter_content(chunk_size=80)):
+            if idx == 1000:
+                return None
+            header_str = header_str + line
+            if line == 'END' + ' '*77:
+                break
+    header = fits.Header.fromstring(header_str)
+    return header
