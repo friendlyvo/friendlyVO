cd6f6aaded5afb559c1f807718bd2d65d29d9fa4
Tom Donaldson <tdonaldson@stsci.edu>
2015-11-05 01:54:10 -0500

    add an equals to fix syntax error

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config.from_object('config')
 
 @app.route('/')
 def hello():
-    page_content 'Hello World! I can deploy automatically :)'
+    page_content = 'Hello World! I can deploy automatically :)'
     return render_template('index.html',
                            page_content = page_content)
 
