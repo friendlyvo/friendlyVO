38b2511135489dd1b85aef16b318441d8b147a7f
James T. Allen <j.allen@physics.usyd.edu.au>
2015-11-05 16:41:50 +1100

    Getting closer to a web form for adding data.

--- scrape.py
+++ scrape.py
@@ -1,25 +1,28 @@
 from astropy.wcs import WCS
 from astropy.wcs.utils import proj_plane_pixel_scales
 
-def extract_image_metadata(header):
+from utils import ImageMetadata
+
+def extract_image_metadata(header, title=None, image_url=None):
     """Return a dictionary of metadata pulled from a fits header."""
     wcs = WCS(header)
-    metadata = {}
-    metadata['title'] = None # required
-    metadata['inst'] = None # recommended
-    metadata['mjd_obs'] = None # recommended
-    metadata['naxes'] = wcs.naxis
-    metadata['naxis'] = (header['NAXIS1'], header['NAXIS2'])
-    metadata['scale'] = proj_plane_pixel_scales(wcs)
-    metadata['format'] = 'image/fits'
-    metadata['crpix'] = wcs.wcs.crpix
-    metadata['crval'] = wcs.wcs.crval
-    metadata['cd'] = wcs.wcs.cd
-    metadata['ref_frame'] = wcs.wcs.radesys
-    metadata['equinox'] = wcs.wcs.equinox
-    # There must surely be a tidier way of doing the next few lines
-    centre = 0.5*(metadata['naxis'][0] - 1), 0.5*(metadata['naxis'][1] - 1)
+    centre = 0.5*(header['NAXIS1'] - 1), 0.5*(header['NAXIS2'] - 1)
     centre_wcs = wcs.wcs_pix2world([centre[0]], [centre[1]], 0)
-    metadata['ra_cen'] = centre_wcs[0][0]
-    metadata['dec_cen'] = centre_wcs[1][0]
+    metadata = ImageMetadata(
+        title=title,
+        ra_center=centre_wcs[0][0],
+        dec_center=centre_wcs[0][1],
+        naxes=wcs.naxis,
+        naxis=(header['NAXIS1'], header['NAXIS2']),
+        scale=proj_plane_pixel_scales(wcs),
+        format='image/fits',
+        image_url=image_url,        
+        )
+    # metadata['inst'] = None # recommended
+    # metadata['mjd_obs'] = None # recommended
+    # metadata['crpix'] = wcs.wcs.crpix
+    # metadata['crval'] = wcs.wcs.crval
+    # metadata['cd'] = wcs.wcs.cd
+    # metadata['ref_frame'] = wcs.wcs.radesys
+    # metadata['equinox'] = wcs.wcs.equinox
     return metadata
