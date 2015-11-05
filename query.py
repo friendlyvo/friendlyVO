c9d33658e6d3d616252a1a6aab70b6ffb392cad7
Tom Donaldson <tdonaldson@stsci.edu>
2015-11-05 05:54:11 -0500

    Region queries!

--- query.py
+++ query.py
@@ -5,5 +5,17 @@ query = Blueprint('query', __name__)
 
 @query.route('/sia')
 def sia():
-    resp = handleSiaRequest(request)
+    
+    
+    posString = request.args.get('POS')
+    if posString is None:
+        posString = ""
+    sizeString = request.args.get('SIZE')
+    if sizeString is None:
+        sizeString = ""
+    formatString = request.args.get('FORMAT')
+    
+    print "pos = {}, size = {}, format = {}".format(posString, sizeString, formatString)
+    resp = handleSiaRequest(posString, sizeString, formatString)
+    
     return resp
